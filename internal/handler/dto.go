e5ad28772902a29da76afa59df384f8823c548ad
devpedrois <pedeoiranildo@gmail.com>
2026-05-02 17:38:31 -0300

    feat(handler): add request/response dtos and json helpers

---
+++ internal/handler/dto.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+)
+
+type ShortenRequest struct {
+	URL string `json:"url"`
+}
+
+type ShortenResponse struct {
+	Hash     string `json:"hash"`
+	ShortURL string `json:"short_url"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+	Code  string `json:"code"`
+}
+
+func WriteJSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		slog.Error("handler: encode response", "err", err)
+	}
+}
+
+func WriteError(w http.ResponseWriter, status int, msg, code string) {
+	WriteJSON(w, status, ErrorResponse{Error: msg, Code: code})
+}
